75033c2435e3b542f001fcfee07a085b57e1df9b
Freddie Hoy <51379192+FreddieHoy@users.noreply.github.com>
2024-08-27 15:00:16 +0100

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[t]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
+  trailingSlash: true,
+};
 
 export default nextConfig;
